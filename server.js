b3a0bf94af6977053bc1783253fe89e3fec66467
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-07-23 17:14:58 +0530

    Home Page Push

--- server.js
+++ server.js
@@ -6,22 +6,50 @@ const path = require('path')
 
 const webServer = http.createServer((req,res) => {
 
-    const filePath = './public' + req.url
-
-    console.log(filePath)
-
-
-    fs.readFile(filePath, (err,data) => {
-
-        if(err) {
-            res.writeHead(404, {'content-type':'text/html'})
-            return res.end()
-        }
-
+    if(req.url === '/') {
+        fs.readFile('./public/home.html', 'UTF-8', (err, data) => {
+            res.writeHead(200, {'content-type':'text/html'})
+            res.write(data)
+            res.end()
+        })
+    }else if(req.url.match('\.css')) {
+        let filePath = path.join(__dirname, 'public', req.url)
+        let fileStream = fs.createReadStream(filePath, 'UTF-8')
+        res.writeHead(200, {'content-type':'text/css'})
+        fileStream.pipe(res)
+        
+    } else if(req.url == "/services.html") {
+        let servicesFile = path.join(__dirname,'public',req.url)
+        let serviceStream = fs.createReadStream(servicesFile, 'UTF-8')
         res.writeHead(200, {'content-type':'text/html'})
-        res.write(data)
-        return res.end()
-    })
+        serviceStream.pipe(res)
+        
+    }else if(req.url.match('\.jpg')) {
+        let jpgPath = path.join(__dirname, req.url)
+        let jpgStream = fs.createReadStream(jpgPath)
+        res.writeHead(200, {'content-type':'image/jpg'})
+        jpgStream.pipe(res)
+    }else if(req.url.match('\.png')) {
+        let jpgPath = path.join(__dirname, req.url)
+        let jpgStream = fs.createReadStream(jpgPath)
+        res.writeHead(200, {'content-type':'image/png'})
+        jpgStream.pipe(res)
+    }
+    
+    // let filePath = path.join(__dirname,'public',req.url)
+
+    // console.log(filePath)
+
+    // let fileStream = fs.createReadStream(filePath, 'UTF-8')
+
+    // res.writeHead(200)
+
+    // fileStream.pipe(res)
+
+
+
+    
+    
 })
 
 webServer.listen(PORT, (err) => {
